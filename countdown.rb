2b0b1fe7cb62e9aaa1224f24d7c3c42313ddaeda
Adjoa Ntoso <adjoaan@gmail.com>
2019-06-18 21:55:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,9 @@ def countdown(element)
 while element > 0
   puts "#{element} SECOND(S)!"
   element -= 1
-  sleep(1)
 end
 "HAPPY NEW YEAR!"
+sleep(1)
 end
 
-countdown_with_sleep(30)
+countdown(30)
