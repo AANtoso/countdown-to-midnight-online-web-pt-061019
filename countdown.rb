ea37f9337109c5ae2045e11a96f9024ba192a04e
Adjoa Ntoso <adjoaan@gmail.com>
2019-06-18 21:35:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
-x = seconds
-while x < 10
-  puts "#{x} SECOND(S)!"
-  x -= 1
-end
-def countdown
+def countdown(element)
+while element < 10
+  puts "#{element} SECOND(S)!"
+  element -= 1
   puts "HAPPY NEW YEAR!"
 end
+
+countdown(30)
